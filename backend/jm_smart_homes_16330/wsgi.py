87ec2b36ad7fec808d3c01aee1c1cd5ba0d9d8d5
Crowdbotics <team@crowdbotics.com>
2020-04-28 03:18:23 +0000

    Update models

--- backend/jm_smart_homes_16330/wsgi.py
+++ backend/jm_smart_homes_16330/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jm_smart_homes_16330.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jm_smart_homes_16330.settings")
 
 application = get_wsgi_application()
